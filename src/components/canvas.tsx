090f4bfdf6d062440e3bf3f8c5ea3eccd3bb0a25
anth0nycodes <hoanganthony2207@gmail.com>
2026-03-12 00:23:09 -0400

    dont add empty text objects to canvas

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -122,6 +122,13 @@ export function Canvas({ currentTool, setCurrentTool }: CanvasProps) {
 
           text.on("editing:exited", () => {
             fc.off({ "mouse:down": handleMouseDown });
+
+            if (text.text === "") {
+              fc.remove(text);
+              fc.discardActiveObject();
+              fc.requestRenderAll();
+            }
+
             text.set({ hasControls: true });
 
             // we use requestAnimationFrame here because Fabric internally clears the active object AFTER the editing:exited event is fired, so without it, it wouldn't actually set the text to be the active object because it would be cleared immediately
